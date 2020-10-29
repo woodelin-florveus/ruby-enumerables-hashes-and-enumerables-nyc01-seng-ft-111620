678e9c70ccbfb7fc57850c723780c0c4394e6f54
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-29 23:04:58 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   winner = ""
   passengers.each do |name, value|
-    if name == name.start_with("A")
+    if name == :suite_a && name.start_with("A")
       winner == name
     end
   end
